b237223c9040d1d14794e498c3c6c8b8c190b4c5
Roman Senderovich <roman.inet@gmail.com>
2026-04-18 20:05:18 -0400

    fix vercel log error

--- middleware.ts
+++ middleware.ts
@@ -7,6 +7,6 @@ export const config = {
   matcher: [
     '/',
     '/(en|fr)/:path*',
-    '/((?!_next|_vercel|api|.*\\..*).*)',
+    '/((?!_next|_vercel|api|icon|apple-icon|favicon|manifest|robots|sitemap|.*\\..*).*)',
   ],
 };
