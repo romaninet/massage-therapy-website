40588650e7cac8221dc246b39b575196a4e9bb98
romaninet <roman.inet@gmail.com>
2026-04-26 00:46:06 -0400

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,6 @@
 export const SITE = {
   url: 'https://www.shelestwellness.ca',
-  lastUpdated: new Date('2026-04-25'),
+  lastUpdated: new Date('2026-04-26'),
   heroBgImage: '/images/image1.jpg',
   ogImage: '/images/media-opengraph.jpg',
   portraitImage: '/images/olya-pic.jpg',
